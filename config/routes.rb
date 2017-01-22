e1004cb16d02cd76c2f85a47e38edd0aabeae44d
Jacob Hedengren <=>
2017-01-22 14:34:25 +0100

    added employers, jobs, contact, for_employers and about controllers index action and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :employers
+  resources :jobs
+  resources :contact
+  resources :for_employers
+  resources :about
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
